f169a76bf561e65b6c94b413f37e766be4763941
yyuuiikk <yutaikawa0315@gmail.com>
2019-01-26 22:16:27 +0900

    ユーザーの登録処理を作成

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -34,21 +36,21 @@ class User implements UserInterface
 	private $password;
 
 	public function getId(): ?int
-	{
-		return $this->id;
-	}
+      	{
+      		return $this->id;
+      	}
 
 	public function getEmail(): ?string
-	{
-		return $this->email;
-	}
+      	{
+      		return $this->email;
+      	}
 
 	public function setEmail(string $email): self
-	{
-		$this->email = $email;
-
-		return $this;
-	}
+      	{
+      		$this->email = $email;
+      
+      		return $this;
+      	}
 
 	/**
 	 * A visual identifier that represents this user.
@@ -56,58 +58,58 @@ class User implements UserInterface
 	 * @see UserInterface
 	 */
 	public function getUsername(): string
-	{
-		return (string) $this->email;
-	}
+      	{
+      		return (string) $this->email;
+      	}
 
 	/**
 	 * @see UserInterface
 	 */
 	public function getRoles(): array
-	{
-		$roles = $this->roles;
-		// guarantee every user at least has ROLE_USER
-		$roles[] = 'ROLE_USER';
-
-		return array_unique($roles);
-	}
+      	{
+      		$roles = $this->roles;
+      		// guarantee every user at least has ROLE_USER
+      		$roles[] = 'ROLE_USER';
+      
+      		return array_unique($roles);
+      	}
 
 	public function setRoles(array $roles): self
-	{
-		$this->roles = $roles;
-
-		return $this;
-	}
+      	{
+      		$this->roles = $roles;
+      
+      		return $this;
+      	}
 
 	/**
 	 * @see UserInterface
 	 */
 	public function getPassword(): string
-	{
-		return (string) $this->password;
-	}
+      	{
+      		return (string) $this->password;
+      	}
 
 	public function setPassword(string $password): self
-	{
-		$this->password = $password;
-
-		return $this;
-	}
+      	{
+      		$this->password = $password;
+      
+      		return $this;
+      	}
 
 	/**
 	 * @see UserInterface
 	 */
 	public function getSalt()
-	{
-		// not needed when using the "bcrypt" algorithm in security.yaml
-	}
+      	{
+      		// not needed when using the "bcrypt" algorithm in security.yaml
+      	}
 
 	/**
 	 * @see UserInterface
 	 */
 	public function eraseCredentials()
-	{
-		// If you store any temporary, sensitive data on the user, clear it here
-		// $this->plainPassword = null;
-	}
+      	{
+      		// If you store any temporary, sensitive data on the user, clear it here
+      		// $this->plainPassword = null;
+      	}
 }

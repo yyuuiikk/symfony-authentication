074727413d59f64900a4a78c0c079d8e824a7963
yyuuiikk <yutaikawa0315@gmail.com>
2019-01-28 23:46:49 +0900

    フロントエンドのライブラリをインストール

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,32 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+// loads the jquery package from node_modules
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+//var $ = require('jquery');
+
+// import the function from greet.js (the .js extension is optional)
+// ./ (or ../) means to look for a local file
+import greet from './greet'
+//var greet = require('./greet');
+
+
+$(document).ready(function() {
+    $('body').prepend('<h1>'+greet('jill')+'</h1>');
+});
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,8 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('checkout', './assets/js/checkout.js')
+    .addEntry('account', './assets/js/account.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -39,7 +41,7 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
